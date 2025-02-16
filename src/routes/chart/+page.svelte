740b81e1b14a66183e23e6bb01964f633767cf6c
tokoba <t.okoba@gmail.com>
2025-02-16 17:43:41 +0900

    finally working well

--- src/routes/chart/+page.svelte
+++ src/routes/chart/+page.svelte
@@ -3,9 +3,9 @@
   import { Chart, type ECMouseEvent } from 'svelte-echarts'
   import { use } from 'echarts/core'
   import EChart from './EChart.svelte'
-  import type { EChartsOption, SetOptionOpts } from 'echarts'
+  import type { EChartsOption, EChartsInitOpts, SetOptionOpts } from 'echarts'
   import { CanvasRenderer } from 'echarts/renderers'
-  import { BarChart } from 'echarts/charts'
+  import { BarChart, LineChart } from 'echarts/charts'
   import {
     DatasetComponent,
     GridComponent,
@@ -29,9 +29,12 @@
     timestamp: Date
     value: number
   }
+  type DataArray = Data[]
 
-  let data: Data[] = $state<Data[]>([])
+  let data: DataArray = $state<DataArray>([])
   let options: EChartsOption = $state({})
+  let initOptions: EChartsInitOpts = $state({})
+  let optionalOptions: SetOptionOpts = $state({})
   let interval: number = 1000
   let numOfData: number = 10
 
@@ -44,7 +47,7 @@
   }
 
   /* data generator */
-  const initializeData = (numberOfDataPoints: number, interval: number = 1000): Data[] => {
+  const initializeData = (numberOfDataPoints: number, interval: number = 1000): DataArray => {
     return Array.from({ length: numberOfDataPoints }, (_, i) => ({
       timestamp: new Date(Date.now() - (numberOfDataPoints - i) * interval),
       value: Math.random() * 100,
@@ -58,33 +61,46 @@
       value: Math.random() * 100,
     }
     data = [...data.slice(1), newData]
-    // console.log('updated data:', $state.snapshot(data))
     updateOptions(data)
-    options = options
-    // console.log('updated options:', $state.snapshot(options))
   }
 
-  // オプション更新関数の定義
-  const updateOptions = (data: Data[]): void => {
-    /**
-     * ECharts用のオプションオブジェクトを生成する関数
-     * @param data - チャートに表示するデータ配列
-     * @returns ECharts用のオプションオブジェクト
-     */
+  // functions for options
+  const initializeOptions = (data: DataArray): EChartsInitOpts => {
+    return {
+      devicePixelRatio: window.devicePixelRatio,
+      renderer: 'svg',
+      ssr: false,
+      useDirtyRect: false,
+      useCoarsePointer: true,
+      pointerSize: 10,
+      width: 'auto',
+      height: 'auto',
+      locale: 'EN',
+    }
+  }
+
+  // ref: https://echarts.apache.org/en/api.html#echartsInstance.setOption
+  const initializeOptionalOptions = (): SetOptionOpts => {
+    return {
+      notMerge: true,
+      replaceMerge: 'series',
+      lazyUpdate: false,
+      silent: false,
+    }
+  }
+
+  // options should be updated with data array
+  const updateOptions = (data: DataArray): void => {
     options = {
       title: {
-        text: 'ECharts example',
+        text: 'ECharts bar chart example',
       },
       xAxis: {
         type: 'category',
         axisLabel: {
-          formatter: (value) =>
-            new Date(value).toLocaleTimeString('en-US', {
-              hour12: false,
-              hour: '2-digit',
-              minute: '2-digit',
-              second: '2-digit',
-            }),
+          formatter: (value) => {
+            return value
+          },
         },
         data: data.map(({ timestamp }) =>
           timestamp.toLocaleTimeString('en-US', {
@@ -105,21 +121,19 @@
         },
       ],
     }
-    console.log('option no title: ', options.title)
-    if (options.series) {
-      console.log('option no series: ', options.series)
-    }
   }
+
+  $effect(() => {
+    // console.log('effect: options:', $state.snapshot(options), 'data', $state.snapshot(data))
+  })
 
   onMount(() => {
     console.log('+page.svelte is mounted')
     data = initializeData(numOfData, interval)
-    console.log('initialized data:', $state.snapshot(data))
+    initOptions = initializeOptions(data)
+    optionalOptions = initializeOptionalOptions()
     const id = setInterval(updateData, interval)
   })
 </script>
 
-<EChart {options} {data} {onclick} {onkeydown} />
-<p>{options.title}</p>
-<p>{data.map((d) => d.value)}</p>
-<p>{data.map((d) => d.timestamp)}</p>
+<EChart {options} {initOptions} {optionalOptions} {data} {onclick} {onkeydown} />
--- src/routes/chart/EChart.svelte
+++ src/routes/chart/EChart.svelte
@@ -1,23 +1,92 @@
 <script lang="ts">
-  let { options, data, onclick, onkeydown } = $props()
-  console.log('options:', options, 'data:', data)
+  import type {
+    EChartsType as BaseEchartsType,
+    EChartsOption,
+    EChartsInitOpts,
+    EChartsType,
+    SetOptionOpts,
+  } from 'echarts'
+  import type { EChartsType as CoreEchartsType } from 'echarts/core'
+  import { init, use } from 'echarts/core'
+  import { onMount } from 'svelte'
+  import { ThemeRiverChart } from 'echarts/charts'
+  import { CanvasRenderer } from 'echarts/renderers'
+  import { BarChart } from 'echarts/charts'
+  import {
+    DatasetComponent,
+    GridComponent,
+    TitleComponent,
+    TooltipComponent,
+    TransformComponent,
+  } from 'echarts/components'
+
+  let { options, initOptions, optionalOptions, data, onclick, onkeydown } = $props()
+  // define the element in which echarts will draw charts
+  let elementId: string = $state('chart')
+  let element: HTMLDivElement | null = null
+
+  // chart component created by EChart
+  let chart: CoreEchartsType | undefined = undefined
+  let theme: string | object | null = 'dark'
+
+  use([
+    BarChart,
+    DatasetComponent,
+    GridComponent,
+    TooltipComponent,
+    TransformComponent,
+    CanvasRenderer,
+    TitleComponent,
+  ])
+
+  const initChart = (elementId: string, initOpt: EChartsInitOpts) => {
+    element = document.getElementById(elementId) as HTMLDivElement | null // type assertion required
+    if (element && initOpt) {
+      chart = init(element, theme, initOpt)
+      console.log('chart is initialized')
+      chart.setOption(initOpt)
+    }
+  }
+
+  onMount(() => {
+    initChart(elementId, initOptions)
+  })
+
+  $effect(() => {
+    if (chart) {
+      chart.setOption(options, optionalOptions)
+    }
+  })
 </script>
 
+<div id={elementId} class="chart-area"></div>
+
 <div
-  class="chart-area"
+  class="chart-debug-viewer"
   {onclick}
   {onkeydown}
   aria-label="interactive chart"
   role="button"
   tabindex="0"
 >
-  <h2 class="chart-title">Here is the chart</h2>
-  <p>{data.map((d) => d.value)}</p>
-  <p>{data.map((d) => d.timestamp)}</p>
+  <h2 class="chart-title">Chart debug viewer</h2>
+  <div>
+    Options: <pre>{JSON.stringify(options, null, 2)}</pre>
+  </div>
+  <div>
+    Data (first 5 items): <pre>{JSON.stringify(data.slice(0, 10), null, 2)}</pre>
+  </div>
 </div>
 
 <style>
   .chart-area {
+    background-color: #cccccc;
+    padding: 50px;
+    border-radius: 20px;
+    width: 800px;
+    height: 800px;
+  }
+  .chart-debug-viewer {
     background-color: #dddddd;
     padding: 30px;
     border-radius: 10px;
